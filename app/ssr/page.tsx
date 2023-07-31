d5ab64a515f0e54dca8b4c889560277f4d772fe0
datvt <vutuandat811@gmail.com>
2023-07-31 14:31:22 +0700

    commit

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -12,7 +12,7 @@ type Users = {
 // }
 
 const getData = async () => {
-    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list")
+    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list", {cache: "no-store"})
     return await res.json();
 }
 
