0bef5a580c1f7cbd5c83222afb1df19fdc0299b0
datvt <vutuandat811@gmail.com>
2023-07-31 14:27:08 +0700

    commit

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -15,8 +15,17 @@ const getData = async () => {
     const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list")
     return await res.json();
 }
+
+export async function generateMetadata({params, searchParams}: { params: any, searchParams: any }) {
+    console.log("MMMMMMMMMMM", params, searchParams)
+    return {
+        title: '...',
+    }
+}
+
 export default async function SSR() {
     let users = await getData()
+    console.log("KKKKKKKKKKKKKkk", users)
     // const {data} = props
     return (
         <div>
