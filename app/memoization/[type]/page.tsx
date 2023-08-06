eacffe470d8403056395db9190272d8aa7b6aea1
DatVTd <thainga0909@gmail.com>
2023-08-07 01:30:51 +0700

    commit

--- app/memoization/[type]/page.tsx
+++ app/memoization/[type]/page.tsx
@@ -1,17 +1,18 @@
 import ComponentA from "@/app/memoization/[type]/ComponentA";
 import ComponentB from "@/app/memoization/[type]/ComponentB";
+import {Suspense} from "react";
 
 export default async function Page({params}: { params: { type: string } }) {
     const {type} = params
     return (
-        // <Suspense fallback={<h1>Loading....</h1>}>
-        <>
+        <Suspense fallback={<h1>Loading....</h1>}>
+            {/*<>*/}
             <h1>{type}</h1>
             <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
                 <ComponentA type={type}/>
                 <ComponentB type={type}/>
             </div>
-        </>
-        // </Suspense>
+            {/*</>*/}
+        </Suspense>
     )
 }
