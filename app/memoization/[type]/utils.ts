0c7dce80c4151345d3aaed32501a578d9335853d
datvt <vutuandat811@gmail.com>
2023-08-07 08:54:45 +0700

    commit

--- app/memoization/[type]/utils.ts
+++ app/memoization/[type]/utils.ts
@@ -1,10 +1,10 @@
 export const getData = async () => {
-    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list")
+    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list", {cache: "no-store"})
     return await res.json();
 }
 
 export const getDataWithSignal = async () => {
     const {signal} = new AbortController()
-    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list", {signal})
+    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list", {signal, cache: "no-store"})
     return await res.json();
 }
