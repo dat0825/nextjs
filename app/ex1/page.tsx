0755df17e2bdcc2f716b80541bfd8d7d04bdbe1c
DatVTd <thainga0909@gmail.com>
2023-08-08 01:27:38 +0700

    commit

--- app/ex1/page.tsx
+++ app/ex1/page.tsx
@@ -1,14 +1,51 @@
-import ClientComponent from "@/app/ex1/ClientComponent";
-import ServerComponent from "@/app/ex1/ServerComponent";
+'use client'
 
-export default async function Page(props: any) {
-    const component = await ServerComponent()
-    console.log("ZZZZZZ", props)
+import React, {Suspense, useEffect, useState} from "react";
+
+export type User = {
+    name: string,
+}
+type Users = {
+    users: Array<User>
+}
+
+type Params = {
+    params: {
+        type: string
+    }
+}
+const getDataWithNoCache = async () => {
+    const res = await fetch("https://64c24343eb7fd5d6ebcf84d0.mockapi.io/list", {cache: "no-store"})
+    return await res.json();
+}
+
+export default function Page() {
+
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        const getData = async () => {
+            let users = await getDataWithNoCache()
+            setUsers(users)
+        }
+        getData()
+    }, [])
+
     return (
-        <div>
-            <ClientComponent
-                component={component}
-            />
-        </div>
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <h1>{"No Cache"}</h1>
+            <div style={{marginTop: 32}}>
+                <ul>
+                    {
+                        users.map((user: User, index: number) => {
+                            return (
+                                <li key={index} style={{marginBottom: 16}}>
+                                    {user.name}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </Suspense>
     )
 }
